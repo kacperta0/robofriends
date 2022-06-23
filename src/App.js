8fbc9653dda2539402e7a2d57f766fa8c09f5be2
kacperta0 <kacpertaurzeski@interia.pl>
2022-06-23 15:46:46 +0200

    Back to normal

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadeee.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
